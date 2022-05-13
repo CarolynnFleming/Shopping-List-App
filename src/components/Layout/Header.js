58974c71135ec014305e34af6959898a0ebcaad1
CarolynnFleming <cfcatpink@gmail.com>
2022-05-13 13:14:27 -0700

    returning a header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,7 +2,7 @@ import { useListitems } from "../../context/ShoppinglistProvider"
 
 export default function Header() {
     const { listitems } = useListitems();
-  return (
-    <div>Header</div>
-  )
+  return
+    <div>Number of List Items left: {listitems.length}</div>;
+  
 }
