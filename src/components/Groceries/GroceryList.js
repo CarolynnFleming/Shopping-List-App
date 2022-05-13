ccc587a23c81636a386396ba8699276547ddbaf9
CarolynnFleming <cfcatpink@gmail.com>
2022-05-13 13:18:36 -0700

    set state

--- src/components/Groceries/GroceryList.js
+++ src/components/Groceries/GroceryList.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useState } from 'react'
 
-export default function GroceryList() {
+export default function GroceryList({ listitem, onUpdate, onDelete }) {
+    const [change, setChange] = useState(false);
+
+    let item:
+
+    if (change) {
+        item = (
+            <form
+
+        )
+    } else {
+
+    }
   return (
     <div>GroceryList</div>
   )
