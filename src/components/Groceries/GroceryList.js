0a333987a2a0fa31b8a9951fd8b6d0daabd0c88f
CarolynnFleming <cfcatpink@gmail.com>
2022-05-13 13:23:49 -0700

    form that alows for editing

--- src/components/Groceries/GroceryList.js
+++ src/components/Groceries/GroceryList.js
@@ -3,12 +3,26 @@ import { useState } from 'react'
 export default function GroceryList({ listitem, onUpdate, onDelete }) {
     const [change, setChange] = useState(false);
 
-    let item:
+    let item;
 
     if (change) {
         item = (
             <form
-
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    setChange(false);
+                }}>
+                    <input
+                    value={listitem.text}
+                    aria-label='edit'
+                    onChange={(event) => {
+                        onUpdate({
+                            ...listitem,
+                            text: event.target.value,
+                        });
+                    }}
+                    />
+                </form>
         )
     } else {
 
