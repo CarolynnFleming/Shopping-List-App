b97c0e4b6eb468635773ef574220e9f779ba329d
CarolynnFleming <cfcatpink@gmail.com>
2022-05-13 13:43:25 -0700

    created state

--- src/views/ShoppingList/List.js
+++ src/views/ShoppingList/List.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import { useState } from "react";
+import GroceryList from "../../components/Groceries/GroceryList";
+import { useListitems } from "../../context/ShoppinglistProvider";
 
 export default function List() {
+    const [newItem, setNewItem] = useState('');
+    const { listitems, controlAddListitems, controlUpdateListitem, controlDeleteListitem } =
+    useListitems();
   return (
     <div>List</div>
   )
