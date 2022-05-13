a852c31f8155086981f284279872a9b62f2ec479
CarolynnFleming <cfcatpink@gmail.com>
2022-05-13 12:28:20 -0700

    components wrapped in provider

--- src/context/ShoppinglistProvider.js
+++ src/context/ShoppinglistProvider.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import Header from './components/Layout/Header';
+import { ShoppinglistProvider } from './context/ShoppinglistProvider';
+import List from './views/ShoppingList/List';
 
 export default function ShoppinglistProvider() {
   return (
-    <div>ShoppinglistProvider</div>
+    <>
+    <ShoppinglistProvider>
+        <Header />
+        <List />
+    </ShoppinglistProvider>
+    </>
   )
 }
